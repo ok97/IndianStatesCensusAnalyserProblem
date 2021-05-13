6eaf4f83b00f534de4b7c0c172db8a6cc3f4a162
ok97 <omkhawshi0@gmail.com>
2021-05-13 23:26:15 +0530

    Added: Code and Testcase: 2 Load indian Census CSV file

--- Indian States Census Analyser Problem Test/UnitTest1.cs
+++ Indian States Census Analyser Problem Test/UnitTest1.cs
@@ -6,6 +6,7 @@ namespace Indian_States_Census_Analyser_Problem_Test
     public class Tests //test class
     {
         static string CSVFilePath = @"D:\Practice\C#\Indian States Census Analyser Problem\Indian States Census Analyser Problem\CSVFiles\IndiaStateCensusData.csv";
+        static string InvalidFilePath = @"D:\Practice\C#\Indian States Census Analyser Problem\Indian States Census Analyser Problem\CSVFiles\IndiaStateCensusData.csv";
         
         CensorAnalyser censusAnalyser;
 
@@ -24,7 +25,23 @@ namespace Indian_States_Census_Analyser_Problem_Test
            int ExpectedCount = 29; //initilise count value
             string[] actual = censusAnalyser.LoadIndianCensusCSVData(CSVFilePath); // acces file csv
             Assert.AreEqual(ExpectedCount, actual.Length); //match
-        }        
+        }
+
+        /* TC1.2:- Given the State Census CSV File if incorrect Returns a custom Exception.
+                   This is a Sad Test Case to verify if the exception is raised. 
+         */
+        [Test]
+        public void GivenIndianCensusCSVFile_WhenFileNotFound_ShouldThrowException()
+        {
+            try
+            {
+                censusAnalyser.LoadIndianCensusCSVData(InvalidFilePath);
+            }
+            catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(CensusAnalyserException.ExceptionType.FILE_NOT_FOUND, ex.type);
+            }
+        }
 
     }
 }
